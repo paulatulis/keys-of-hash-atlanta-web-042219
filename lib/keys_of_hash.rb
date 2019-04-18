1cb5915ff3abf3a63371189f4ab1b9372af39b51
Paula Tulis <tulis.paula@gmail.com>
2019-04-18 16:54:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+def arguments 
+  animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+end 
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    hash.each do |key, value|
+      if value == arguments 
+      puts key 
   end
 end
+end 
+end 

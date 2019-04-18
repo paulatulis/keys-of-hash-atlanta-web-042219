801f4a5ef1eeaa44e3e2d23b000ef6a86e138ae1
Paula Tulis <tulis.paula@gmail.com>
2019-04-18 17:01:40 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,4 @@
-def arguments 
-  animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
-end 
+
 
 class Hash
   def keys_of(*arguments)
